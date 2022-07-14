0dc4f22c8e12e2b8072a4af2fd4d6f59a7324498
jvelazquez <jonathanvgms@gmail.com>
2022-07-14 10:20:03 -0300

    Initial commit

---
+++ Persona.Biblioteca/Persona1.cs
@@ -0,0 +1,58 @@
+﻿namespace Persona.Biblioteca;
+public class Persona1
+{
+    public string Nombre { get; private set; }
+    public int Edad { get; private set; }
+    public float Peso { get; private set; }
+    public float Altura { get; private set; }
+    public Persona1(string nombre, int edad, float peso, float Altura)
+    {
+        this.Nombre = nombre;
+        this.Edad = edad;
+        this.Peso = peso;
+        this.Altura = Altura;
+    }
+
+    public string CualEsTuIMC()
+    {
+        float imc = IMC();
+
+        if (imc < 18.5)
+            return "Bajo peso";
+        else
+            if (imc >= 18.5 && imc < 25)
+            return "Peso ideal";
+        else
+            return "Sobrepeso";
+    }
+
+    private float IMC() => this.Peso / (this.Altura * this.Altura);
+
+    public bool EsMayorDeEdad()
+    {
+        // if (this.Edad >= 18)
+        //     return true;
+        // else
+        //     return false;
+
+        // if (this.Edad >= 18)
+        //     return true;
+
+        // return false;
+
+        return this.Edad >= 18 ? true : false;
+    }
+
+    [Obsolete("Método no respeta la responsabilidad de la clase")]
+    public void Informar()
+    {
+        Console.WriteLine($"Nombre: {this.Nombre}");
+        Console.WriteLine($"Edad: {this.Edad}");
+        Console.WriteLine($"Peso: {this.Peso}, Altura: {this.Altura}");
+    }
+
+    public override string ToString()
+    {
+        return $"Nombre: {this.Nombre}, Edad: {this.Edad}, Peso: {this.Peso}, Altura: {this.Altura}";
+    }
+}
---
+++ Persona.Consola/Program.cs
@@ -0,0 +1,24 @@
+﻿using Persona.Biblioteca;
+
+Console.Write("Ingrese Nombre: ");
+string nombre = Console.ReadLine();
+Console.Write("Ingrese Edad: ");
+int edad = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ingrese Peso: ");
+float peso = float.Parse(Console.ReadLine());
+Console.Write("Ingrese Altura: ");
+float altura = float.Parse(Console.ReadLine());
+
+//caso 1
+//Persona1 persona = new Persona1(nombre, edad, peso, altura);
+
+//caso 2
+//var persona = new Persona1(nombre, edad, peso, altura);
+
+//caso 3
+Persona1 persona = new(nombre, edad, peso, altura);
+
+Console.WriteLine($"El estado de peso de {persona.Nombre} es {persona.CualEsTuIMC()}");
+string estado = persona.EsMayorDeEdad() ? "Si" : "No";
+Console.WriteLine($"Es mayor de edad: {estado}");
+Console.WriteLine(persona.ToString());
